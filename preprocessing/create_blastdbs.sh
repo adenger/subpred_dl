a6d0cb63ccf7cabb4cb881d4c213e42498a8c360
Andreas Denger <a.denger1@gmail.com>
2025-05-11 16:16:31 +0200

    uncomment

--- preprocessing/create_blastdbs.sh
+++ preprocessing/create_blastdbs.sh
@@ -4,5 +4,5 @@ gunzip -c data/raw/uniref/uniref50/uniref50.fasta.gz > data/datasets/blastdb/uni
 cd data/datasets/blastdb/uniref50 && makeblastdb -in uniref50.fasta -parse_seqids -dbtype prot
 
 echo "Extracting Uniref90..."
-# gunzip -c data/raw/uniref/uniref90/uniref90.fasta.gz > data/datasets/blastdb/uniref90/uniref90.fasta
-# cd data/datasets/blastdb/uniref90 && makeblastdb -in uniref90.fasta -parse_seqids -dbtype prot
+gunzip -c data/raw/uniref/uniref90/uniref90.fasta.gz > data/datasets/blastdb/uniref90/uniref90.fasta
+cd data/datasets/blastdb/uniref90 && makeblastdb -in uniref90.fasta -parse_seqids -dbtype prot
