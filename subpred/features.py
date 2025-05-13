215bbfec16331c573c80a2db3e63d4480d808239
Andreas Denger <a.denger1@gmail.com>
2025-05-13 02:08:10 +0200

    rename

--- subpred/features.py
+++ subpred/features.py
@@ -11,7 +11,7 @@ from sklearn.preprocessing import scale
 def calculate_features(
     series_sequences: pd.Series,
     standardize_samples: bool = False,
-    pssm_folder: str = "../data/datasets/blast/",
+    pssm_folder: str = "../data/datasets/pssm/",
     blastdb_folder: str = "../data/datasets/blastdb/",
     verbosity_pssm:int=1
 ):
