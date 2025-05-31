572373a15332236cf2c53192403888519ef9b603
Andreas Denger <a.denger1@gmail.com>
2025-05-31 03:33:59 +0200

    backup command

--- backup.sh
+++ backup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-tar cf - data/raw | pigz > backup_rawdata.tar.gz
-tar cf - data/datasets | pigz > backup_datasets.tar.gz
+mkdir backup
+tar cf - data/raw | pigz > backup/backup_rawdata.tar.gz
+tar cf - data/datasets | pigz > backup/backup_datasets.tar.gz
+tar --exclude=".git" --exclude="data" --exclude="build" --exclude="backup_code.tar" -cf backup/backup_code.tar .
